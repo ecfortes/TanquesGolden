67151ca898f79eaf7c60dcf643cfcecc724261ea
ecfortes <98495750+ecfortes@users.noreply.github.com>
2024-06-19 22:36:02 -0300

    modified:   config/db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-const pg = require('pg');
 
+const pg = require('pg');
 const config = {
   user: 'postgres',    // Seu nome de usu�rio do PostgreSQL
   database: 'postgres', // Nome do banco de dados
@@ -7,7 +7,5 @@ const config = {
   host: '35.231.149.126',    // Host do banco de dados
   port: 5432,           // Porta padr�o do PostgreSQL
 };
-
 const pool = new pg.Pool(config);
-
-module.exports = pool;
+module.exports = pool
