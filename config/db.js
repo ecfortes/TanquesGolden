7324208945d18f47a1b389c3554fcbb5ed7dcda2
ecfortes <98495750+ecfortes@users.noreply.github.com>
2024-06-19 21:50:51 -0300

    modified:   config/db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const pg = require('pg');
 const config = {
   user: 'postgres',    // Seu nome de usu�rio do PostgreSQL
   database: 'postgres', // Nome do banco de dados
-  password: '1234567890', // Sua senha do PostgreSQL
+  password: 'postgres', // Sua senha do PostgreSQL
   host: 'localhost',    // Host do banco de dados
   port: 5432,           // Porta padr�o do PostgreSQL
 };
